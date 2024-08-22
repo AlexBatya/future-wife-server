cb5f0447327bf7d38a8416dc3e40da773f6d8e77
AlexBatya <alex28.12.12@mail.ru>
2024-08-22 18:36:00 +1000

    'new'

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,9 +1,20 @@
 const request = require('supertest');
 const app = require('../build/app.js').default;
+const auth = require("../build/middleware/auth.middleware.js");
+const config = require('../build/config/localhost.json');
 
 describe("GET /api/guests", () => {
 	it("Должно выдать json. Либо нет гостей либо массив гостей", async () => {
-		const response = await request(app).get('/api/guests');
-		expect(response.json).toBe();
+		const response = await request(app)
+			.get('/api/guests')
+			.set("Authorization", config.server.token)
+		expect(response.status).toBe(200);
+	})
+
+	it("Должен находится определённый гость по id, либо писать что такого нет", async () => {
+		const response = await request(app)
+			.get('/api/guests/Иван%20Иванов')
+			.set("Authorization", config.server.token)
+		expect(response.status).toBe(200);
 	})
 })
